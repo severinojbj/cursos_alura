9ae0c16a2ce83d424537f1e5640a9a24844fa06f
severino <severino.junior@laceti.cin.ufpe.br>
2021-05-31 16:45:33 -0300

    parte 5 tutorial graphql incompleto

--- node_graphql/api/user/resolvers/userResolvers.js
+++ node_graphql/api/user/resolvers/userResolvers.js
@@ -1,3 +1,5 @@
+const { GraphQLScalarType } = require ('graphql');
+
 // const arrayUsers = [
 //     {
 //       nome: "Ana",
@@ -10,13 +12,32 @@
 // ];
 
 const userResolvers = {
+
+    RolesType: {
+        ESTUDANTE: "ESTUDANTE",
+        DOCENTE: "DOCENTE",
+        COODENACAO: "COODENACAO"
+    },
+    
+    Datetime: new GraphQLScalarType ({
+        name: 'Datetime',
+        description: 'String de data e hora no formato ISO-8611',
+        serialize: (value) => value.toISOString(value),
+        parseValue: (value) => new Date (value),
+        parseLiteral: (ast) => new Date (ast.value)
+    }),
+
     Query: {
         users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
         // firstUser: () => arrayUsers [0]
         user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)
     },
     Mutation: {
-        addUser: (root, user, { dataSources }) => dataSources.usersAPI.addUser(user),
+        addUser: async (root, user, { dataSources }) => 
+        {
+            console.log (user)
+            return dataSources.usersAPI.addUser(user)
+        },
         updateUser: (root, user, { dataSources }) => dataSources.usersAPI.updateUser(user),
         deleteUser: (root, { id }, { dataSources }) => dataSources.usersAPI.deleteUser(id)
     }
